ec5325ee32df656f75b5b3ab3efb965adcbb22da
German Arce <germanalexis999@gmail.com>
2025-04-04 19:38:33 -0300

    add postorder,inorder

---
+++ ArbolBinario.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace TP_1_ARCE_GERMAN
+{
+    public class ArbolBinario<T>
+    {
+        private T dato;
+        private ArbolBinario<T>? hijoIzquierdo;
+        private ArbolBinario<T>? hijoDerecho;
+
+        public ArbolBinario(T dato)
+        {
+            this.dato = dato;
+            this.hijoIzquierdo = null;
+            this.hijoDerecho = null;
+        }
+
+        public T getDatoRaiz()
+        {
+            return this.dato;
+        }
+
+        public ArbolBinario<T>? getHijoIzquierdo()
+        {
+            return this.hijoIzquierdo;
+        }
+
+        public ArbolBinario<T>? getHijoDerecho()
+        {
+            return this.hijoDerecho;
+        }
+
+        public void agregarHijoIzquierdo(ArbolBinario<T> hijo)
+        {
+            this.hijoIzquierdo = hijo;
+        }
+
+        public void agregarHijoDerecho(ArbolBinario<T> hijo)
+        {
+            this.hijoDerecho = hijo;
+        }
+
+        public void eliminarHijoIzquierdo()
+        {
+            this.hijoIzquierdo = null;
+        }
+
+        public void eliminarHijoDerecho()
+        {
+            this.hijoDerecho = null;
+        }
+
+        public bool esHoja()
+        {
+            return this.hijoIzquierdo == null && this.hijoDerecho == null;
+        }
+
+        public void inorden()
+        {
+
+            if (this.hijoIzquierdo != null)
+            {
+                this.hijoIzquierdo.inorden();
+            }
+
+            Console.WriteLine(this.dato);
+
+            if (this.hijoDerecho != null)
+            {
+                this.hijoDerecho.inorden();
+            }
+
+
+        }
+
+        public void preorden()
+        {
+
+            Console.WriteLine(this.dato);
+
+            if (this.hijoIzquierdo != null)
+            {
+                this.hijoIzquierdo.preorden();
+            }
+            if (this.hijoDerecho != null)
+            {
+                this.hijoDerecho.preorden();
+            }
+        }
+
+
+        public void postorden()
+        {
+
+            
+
+            if (this.hijoIzquierdo != null)
+            {
+                this.hijoIzquierdo.postorden();
+            }
+            if (this.hijoDerecho != null)
+            {
+                this.hijoDerecho.postordenn();
+            }
+            Console.WriteLine(this.dato);
+        }
+
+        public void recorridoPorNiveles()
+        {
+        }
+
+        public int contarHojas()
+        {
+            return 0;
+        }
+
+        public bool incluye(T dato) {
+
+            //recorro y veo si el dato esta 
+
+            if (this.dato.Equals(dato))
+            {
+                return true;
+            }
+
+            bool encontrado= false;
+
+
+            if (hijoIzquierdo != null)
+            {
+                encontrado = hijoIzquierdo.incluye(dato);
+            }
+
+            if (!encontrado && hijoDerecho != null)
+            {
+                encontrado = hijoDerecho.incluye(dato);
+            }
+
+            return encontrado;
+
+
+        }
+
+
+
+        public void recorridoEntreNiveles(int n, int m)
+        {
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using TP_1_ARCE_GERMAN;
+
+namespace TP_1_ARCE_GERMAN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //creamos el arbol binario
+            ArbolBinario<int> arbolBinarioA = new ArbolBinario<int>(1);
+            //creamos los hijos del arbol binario
+
+            ArbolBinario<int> hijoIzquierdo = new ArbolBinario<int>(2);
+            hijoIzquierdo.agregarHijoIzquierdo(new ArbolBinario<int>(4));
+            //creamos el hijo derecho del arbol binario
+
+            ArbolBinario<int> hijoDerecho = new ArbolBinario<int>(3);
+            hijoDerecho.agregarHijoIzquierdo(new ArbolBinario<int>(5));
+            hijoDerecho.agregarHijoDerecho(new ArbolBinario<int>(6));
+            //  agregamos los hijos al arbol binario
+
+            arbolBinarioA.agregarHijoIzquierdo(hijoIzquierdo);
+            arbolBinarioA.agregarHijoDerecho(hijoDerecho);
+
+            //llamamos al metodo nuevo
+            ArbolBinario<int> arbolBinarioB = nuevo(arbolBinarioA);
+
+            //imprimimos el arbol binario
+            Console.WriteLine("Arbol binario A");
+            arbolBinarioA.preorden();
+            Console.WriteLine("Arbol binario B");
+            arbolBinarioB.preorden();
+
+
+            //imprimir si incluye un dato
+
+            Console.WriteLine("El arbol binario A incluye el dato 5: " + arbolBinarioA.incluye(-9));
+
+
+        }
+
+        private static ArbolBinario<int> nuevo(ArbolBinario<int> arbol)
+        {
+            //creamos un arbol binario nuevo a partir de un arbol binario dado
+            if (arbol == null)
+            {
+                return null;
+            }
+
+            ArbolBinario<int> nuevoArbol = new ArbolBinario<int>(arbol.getDatoRaiz());
+
+           if (arbol.getHijoIzquierdo() != null)
+            {
+                int dato = arbol.getDatoRaiz() + arbol.getHijoIzquierdo().getDatoRaiz();
+                nuevoArbol.agregarHijoIzquierdo(nuevo(new ArbolBinario<int>(dato)));
+
+            }
+            if (arbol.getHijoDerecho() != null)
+            {
+                int dato = arbol.getHijoDerecho().getDatoRaiz();
+                nuevoArbol.agregarHijoDerecho(nuevo(arbol.getHijoDerecho()));
+            }
+
+            return nuevoArbol;
+        }
+    }
+}
